4be5ccd1ea3f7a9659464b32ca1232e061f4f348
Dheeraj Garg <dheerajgarg.in@gmail.com>
2022-07-07 20:25:38 +0530

    Deploying

--- Ecom/settings.py
+++ Ecom/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fw02@zd++84bpou9kv$l-hm#cegy9=^%kutm**ce4mn^9&jvqw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['e-shop--django.herokuapp.com', 'localhost', '127.0.0.1']
 
@@ -119,8 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR/"static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- Ecom/urls.py
+++ Ecom/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path('shop/', include('shop.urls')),
     path('blog/', include('blog.urls')),
 
-] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
